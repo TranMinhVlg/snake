ba5e72cee151ed4c33a9216b67a77995f32ef0d6
tranminhvlg <villageois.tranminh@hotmail.fr>
2017-03-15 12:12:35 +0100

    🐍  New project : Snake

---
+++ main.js
@@ -0,0 +1,13 @@
+var snake = function()
+{
+  this.posX = posX;
+  this.posY = posY;
+  this.point = point;
+  this.vitesse = vitesse;
+  this.couleur;
+  this.dir;
+  this.keyCode;
+  this.queue;
+  this.bonus;
+  
+  
